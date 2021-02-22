2e12e970a12db06ef8d0ef4583d662d05d058733
FMoller <fredericomollerper@gmail.com>
2021-02-22 16:21:14 -0300

    Update python_test.py

---
+++ line_counter/TestCodes/python_test.py
@@ -0,0 +1,24 @@
+"""A simple file to test the line_counter performance in python"""
+
+__author__ = "Frederico Moeller"
+__copyright__ = ""
+__credits__ = ["Frederico Moeller"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Frederico Moeller"
+__email__ = ""
+__status__ = ""
+
+#import things
+import math
+
+#define things
+def some_function(var_one, var_two,
+                  var_three):
+    if var_one > var_two:
+        if var_two*var_three > var_one:
+            return "blab" #this happens
+        else:
+            return "blob"
+    else:
+        return "fish"
