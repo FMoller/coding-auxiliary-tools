6ae5404c1d70a11f227c073a0a88d00e81964044
FMoller <fredericomollerper@gmail.com>
2021-02-22 16:31:17 -0300

    line_counter 0.1

--- line_counter/line_counter.py
+++ line_counter/line_counter.py
@@ -2,3 +2,12 @@
 
 Read a script and extract useful info  
 """
+
+__author__ = "Frederico Moeller"
+__copyright__ = "Copyright 2021, Frederico Moeller"
+__credits__ = ["Frederico Moeller"]
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Frederico Moeller"
+__email__ = "fredericomollerped@gmail.com"
+__status__ = "Prototype"
