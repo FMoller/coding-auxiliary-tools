fb8a0a590f0511a12bd8bf3b95b2e809fa787a0a
FMoller <fredericomollerper@gmail.com>
2021-02-22 17:02:14 -0300

    Update line_counter.py

--- line_counter/line_counter.py
+++ line_counter/line_counter.py
@@ -7,7 +7,19 @@ __author__ = "Frederico Moeller"
 __copyright__ = "Copyright 2021, Frederico Moeller"
 __credits__ = ["Frederico Moeller"]
 __license__ = "MIT"
-__version__ = "0.0.1"
+__version__ = "0.1"
 __maintainer__ = "Frederico Moeller"
 __email__ = "fredericomollerped@gmail.com"
 __status__ = "Prototype"
+
+
+file_name = "python_test.py"
+file_path = ""
+
+
+def get_ext(f_name):
+    for i in range(len(f_name)-1,-1,-1):
+        if f_name[i]=='.':
+            return f_name[i:]
+    return None
+    
