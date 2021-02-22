bb6f9f6fd9f075b492d7a2d30857c1e469b6faf4
FMoller <fredericomollerper@gmail.com>
2021-02-22 18:05:20 -0300

    Update line_counter.py

--- line_counter/line_counter.py
+++ line_counter/line_counter.py
@@ -12,14 +12,24 @@ __maintainer__ = "Frederico Moeller"
 __email__ = "fredericomollerped@gmail.com"
 __status__ = "Prototype"
 
+import pandas as pd
 
 file_name = "python_test.py"
 file_path = ""
-
+lang_data = pd.read_csv("plang.csv")
+lang_data.set_index('ext')
 
 def get_ext(f_name):
+    """Get the file extension"""
+    
     for i in range(len(f_name)-1,-1,-1):
         if f_name[i]=='.':
             return f_name[i:]
     return None
     
+def get_info(f_path,f_name):
+    ext = get_ext(f_name)
+    try:
+        pass
+    except:
+        raise ValueError("Extension not found in plang.csv")
